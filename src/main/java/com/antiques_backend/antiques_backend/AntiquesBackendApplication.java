cc929f203fca3fd66de4cbeed501096f1558efd7
oscarArismendi <oscarfernandoarismendigalvis@gmail.com>
2024-09-23 12:32:04 -0500

    first commit

---
+++ src/main/java/com/antiques_backend/antiques_backend/AntiquesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.antiques_backend.antiques_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AntiquesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AntiquesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/antiques_backend/antiques_backend/AntiquesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.antiques_backend.antiques_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AntiquesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
